335447e6b9f8484e0d7a4c61b6e6478481bf8e6d
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-05-05 16:23:32 -0400

    feat(data): Created User Class

---
+++ src/main/java/dataobjects/User.java
@@ -0,0 +1,19 @@
+package dataobjects;
+
+public class User {
+    private String login;
+    private long id;
+    private String node_id;
+    private String avatar_url;
+    private String gravatar_url;
+    private String url;
+    private String html_url;
+    private String followers_url;
+    private String following_url;
+    private String gists_url;
+    private String starred_url;
+    private String subscriptions_url;
+    private String organizations_url;
+
+
+}
